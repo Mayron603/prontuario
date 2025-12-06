d88ac811fd3a837ee76940e350b100943b64799e
Mayron Paula <may689645@gmail.com>
2025-12-06 19:07:46 -0300

    Adicionando Model Prontuario com conversao de id

--- server/models/Prontuario.js
+++ server/models/Prontuario.js
@@ -1,55 +1,64 @@
-// server/models/Prontuario.js
 const mongoose = require('mongoose');
-
-const SinalVitalSchema = new mongoose.Schema({
-  data_hora: String,
-  pa: String,
-  fc: Number,
-  fr: Number,
-  temperatura: Number,
-  spo2: Number,
-  dor: Number
-});
-
-const EvolucaoSchema = new mongoose.Schema({
-  data_hora: String,
-  descricao: String,
-  enfermeiro: String
-});
-
-const IntervencaoSchema = new mongoose.Schema({
-  intervencao: String,
-  horario: String,
-  responsavel: String
-});
-
-const PrescricaoSchema = new mongoose.Schema({
-  medicamento: String,
-  dose: String,
-  via: String,
-  horarios: String
-});
 
 const ProntuarioSchema = new mongoose.Schema({
   nome_paciente: { type: String, required: true },
-  idade: Number,
-  sexo: String,
+  idade: { type: Number, required: true },
+  sexo: { type: String, required: true },
   diagnostico_principal: { type: String, required: true },
   diagnosticos_secundarios: [String],
   estado_clinico: String,
   prioridade: String,
   complexidade: String,
   quarto_leito: String,
-  data_internacao: String,
+  data_internacao: Date,
   historia_clinica: String,
   antecedentes: String,
   alergias: [String],
-  sinais_vitais: [SinalVitalSchema],
-  evolucao_enfermagem: [EvolucaoSchema],
-  intervencoes: [IntervencaoSchema],
-  prescricoes: [PrescricaoSchema],
+  sinais_vitais: [
+    {
+      data_hora: String,
+      pa: String,
+      fc: Number,
+      fr: Number,
+      temperatura: Number,
+      spo2: Number,
+      dor: Number
+    }
+  ],
+  evolucao_enfermagem: [
+    {
+      data_hora: String,
+      descricao: String,
+      enfermeiro: String
+    }
+  ],
+  intervencoes: [
+    {
+      intervencao: String,
+      horario: String,
+      responsavel: String
+    }
+  ],
+  prescricoes: [
+    {
+      medicamento: String,
+      dose: String,
+      via: String,
+      horarios: String
+    }
+  ],
   observacoes: String,
   created_date: { type: Date, default: Date.now }
+});
+
+// --- A MÁGICA ACONTECE AQUI ---
+// Essa configuração faz o MongoDB devolver "id" em vez de "_id" para o frontend
+ProntuarioSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id; // Remove o _id original para não ficar duplicado
+  }
 });
 
 module.exports = mongoose.model('Prontuario', ProntuarioSchema);
